e98ee6f9ecf5cd995553525eccaf2d3bbd3c6f4f
Maksym Stankov <maksym.stankov@gmail.com>
2022-12-16 01:19:51 +0200

    fix task # 6

--- js/task-06.js
+++ js/task-06.js
@@ -4,7 +4,8 @@ function onBlur(evt) {
   let inputLenght = evt.currentTarget.value.length;
 
   if (inputLenght == inputEl.dataset.length) {
-    inputEl.classList.replace('invalid', 'valid');
+    inputEl.classList.add('valid');
+    inputEl.classList.remove('invalid');
   } else {
     inputEl.classList.add('invalid');
   }
